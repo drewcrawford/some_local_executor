d52ea926f0a74dda1c9bea70f3d3e53c3c9e263d
Drew Crawford <drew@sealedabstract.com>
2024-10-23 19:30:16 -0500

    Implement some things

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,45 @@ It's a simple single-threaded executor.
 use std::future::Future;
 use std::marker::PhantomData;
 
+struct Task<'a> {
+    future: Box<dyn Future<Output=()> + 'a>
+}
+
+impl<'a> Task<'a> {
+    fn new<F>(future: F) -> Self
+    where
+        F: Future<Output = ()> + 'a
+    {
+        Task {
+            future: Box::new(future)
+        }
+    }
+}
+
 pub struct Executor<'tasks> {
+    tasks: Vec<Task<'tasks>>,
+    //todo: remove
     data: PhantomData<&'tasks ()>
 }
 
 impl<'tasks> Executor<'tasks> {
     pub fn new() -> Self {
-        todo!()
+        Executor {
+            tasks: Vec::new(),
+            data: PhantomData
+        }
     }
     /**
     Spawns the task onto the executor.
     */
     pub fn spawn(&mut self, future: impl Future<Output = ()> + 'tasks) {
-        todo!()
+        self.tasks.push(Task::new(future));
     }
     /**
     Runs the executor if there is work to be performed.
     */
     pub fn do_some(&mut self) {
-
+        todo!()
     }
     /**
     Drains the executor. After this call, the executor can no longer be used.
