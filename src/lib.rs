7a6d09dae9b987deaefb64a2b6a530dbe979dbce
Drew Crawford <drew@sealedabstract.com>
2024-10-27 13:23:23 -0500

    Sketch main API

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,36 @@ It's a simple single-threaded executor.
 
 use std::future::Future;
 use std::marker::PhantomData;
+use std::pin::Pin;
+use std::task::{Context, RawWaker, RawWakerVTable, Waker};
+
+const VTABLE: RawWakerVTable = RawWakerVTable::new(
+    |data| {
+        todo!()
+    },
+    |data| {
+        todo!()
+    },
+    |data| {
+        todo!()
+    },
+    |data| {
+        todo!()
+    }
+);
+
+
+enum RunResult {
+    ///The executor performed no work.
+    DidNothing,
+    ///The executor did some work, and more is available to do.
+    DidSome,
+    /// The executor did some work, and there is no more work to do at present.
+    Done
+}
 
 struct Task<'a> {
-    future: Box<dyn Future<Output=()> + 'a>
+    future: Pin<Box<dyn Future<Output=()> + 'a>>
 }
 
 impl<'a> Task<'a> {
@@ -15,7 +42,7 @@ impl<'a> Task<'a> {
         F: Future<Output = ()> + 'a
     {
         Task {
-            future: Box::new(future)
+            future: Box::pin(future)
         }
     }
 }
@@ -23,14 +50,18 @@ impl<'a> Task<'a> {
 pub struct Executor<'tasks> {
     tasks: Vec<Task<'tasks>>,
     //todo: remove
-    data: PhantomData<&'tasks ()>
+    data: PhantomData<&'tasks ()>,
+    waker: Waker,
 }
 
 impl<'tasks> Executor<'tasks> {
     pub fn new() -> Self {
+
+        let waker = unsafe{Waker::from_raw(RawWaker::new(std::ptr::null(), &VTABLE))};
         Executor {
             tasks: Vec::new(),
-            data: PhantomData
+            data: PhantomData,
+            waker
         }
     }
     /**
@@ -40,10 +71,19 @@ impl<'tasks> Executor<'tasks> {
         self.tasks.push(Task::new(future));
     }
     /**
-    Runs the executor if there is work to be performed.
+    Runs the executor a small amount if there is some work to be performed.
+
     */
-    pub fn do_some(&mut self) {
-        todo!()
+    pub fn do_some(&mut self) -> RunResult {
+        let mut context = Context::from_waker(&self.waker);
+        if let Some(mut task) = self.tasks.pop() {
+            let e = Pin::as_mut(&mut task.future).poll(&mut context);
+            todo!("Add task back to the queue if it's not done");
+
+        }
+        else {
+            RunResult::DidNothing
+        }
     }
     /**
     Drains the executor. After this call, the executor can no longer be used.
